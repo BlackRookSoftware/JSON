e204e562ee92c8443b0f81d4d7450cf1060fc4b6
mtrop <mtropiano@gmail.com>
2019-07-04 20:48:22 -0400

    Fixed Lexer.

--- src/test/java/com/blackrook/json/JSONTest.java
+++ src/test/java/com/blackrook/json/JSONTest.java
@@ -3,6 +3,8 @@ package com.blackrook.json;
 import java.io.InputStream;
 import java.util.Arrays;
 
+import com.blackrook.json.annotation.JSONName;
+
 public final class JSONTest
 {
 	public static void main(String[] args) throws Exception
@@ -10,12 +12,15 @@ public final class JSONTest
 		InputStream in = ClassLoader.getSystemClassLoader().getResourceAsStream("com/blackrook/json/test.json");
 		Pair[] out = JSONReader.readJSON(Pair[].class, in);
 		System.out.println(Arrays.toString(out));
+		System.out.println(JSONWriter.writeJSONString(out));
 	}
 	
 	public static class Pair
 	{
 		public int x;
 		public int y;
+		@JSONName("name")
+		public int a;
 		private int z;
 		
 		public int getZ()
@@ -31,7 +36,7 @@ public final class JSONTest
 		@Override
 		public String toString()
 		{
-			return "(" + x + ", " + y + ", " + z + ")";
+			return "(" + x + ", " + y + ", " + z + ", " + a + ")";
 		}
 	}
 }
